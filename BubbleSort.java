468d3a649a3924f12ccc8b314d0581b554558471
highzor <52636901+highzor@users.noreply.github.com>
2020-12-01 15:41:43 +0300

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        // O(n^2)
+        int[] array = {10, 2, 10, 3, 1, 2, 5};
+        System.out.println(Arrays.toString(array));
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i-1]) {
+                    swap(array, i, i-1);
+                    needIteration = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    
+    public static void swap(int[] array, int ind1, int ind2) {
+        int temp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = temp;
+    }
+}
